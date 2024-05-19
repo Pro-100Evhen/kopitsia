b104ea1b8ae28265594b17076f57206c5ccb34ce
Evhen Nazarenko <Evgen1215@ukr.net>
2024-05-19 14:07:22 +0300

    +lockal stpreg info

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
+import {
+   persistReducer,
+   FLUSH,
+   REHYDRATE,
+   PAUSE,
+   PERSIST,
+   PURGE,
+   REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+
 import ProductSlice from "./slices/ProductSlice";
 import CartSlice from "./slices/CartSlice";
 
-const store = configureStore({
-   reducer: {
-      products: ProductSlice,
-      cart: CartSlice,
-   },
+const rootReducer = combineReducers({
+   products: ProductSlice,
+   cart: CartSlice,
 });
 
+const persistConfig = {
+   key: "root",
+   storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({
+   reducer: persistedReducer,
+   middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+         serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+         },
+      }),
+});
+
+export const persistor = persistStore(store);
 export default store;
