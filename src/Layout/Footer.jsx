ba42b6bdcdb483e79de98d3f5e5a7849d211daf5
Evhen <evgen1215@ukr.net>
2025-12-04 11:03:34 +0200

    Refactor code structure for improved readability and maintainability

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -13,78 +13,7 @@ const Footer = () => {
             <div className="container">
                <div className="row gy-4">
                   <div className="footer-info__conteiner">
-                     <h3>Розроблено в якості пет-проєкту</h3>
-
-                     <h5 className="footer-info__contacts-title">
-                        Доступні контакти:
-                     </h5>
-                     <ul className="footer-info__contacts-list">
-                        <li className="footer-info__contact-li">
-                           <span className="footer-info__contact-source">
-                              <SiTelegram />
-                              Telegram:
-                           </span>
-                           <span className="footer-info__contact-value">
-                              <a href="https://t.me/Faara_on">@Faara_on</a>
-                           </span>
-                        </li>
-                        <li className="footer-info__contact-li">
-                           <span className="footer-info__contact-source">
-                              <FaPhone />
-                              Phone:
-                           </span>
-                           <span className="footer-info__contact-value">
-                              <a href="tel:+380994524923">+380 99 452 49 23</a>
-                           </span>
-                        </li>
-                        <li className="footer-info__contact-li">
-                           <span className="footer-info__contact-source">
-                              <FaGithub />
-                              Github:
-                           </span>
-                           <span className="footer-info__contact-value">
-                              <a href="https://github.com/Pro-100Evhen">
-                                 https://github.com/Pro-100Evhen
-                              </a>
-                           </span>
-                        </li>
-                        <li className="footer-info__contact-li">
-                           <span className="footer-info__contact-source">
-                              <IoMdMailUnread />
-                              email:
-                           </span>
-                           <span className="footer-info__contact-value">
-                              <a href="email:zenanazarenko652@gmail.com">
-                                 zenanazarenko652@gmail.com
-                              </a>
-                           </span>
-                        </li>
-                        <li className="footer-info__contact-li">
-                           <span className="footer-info__contact-source">
-                              <FaLinkedin />
-                              Linkedin:
-                           </span>
-                           <span className="footer-info__contact-value">
-                              <a href="https://www.linkedin.com/in/yevhen-nazarenko-b2b731266/">
-                                 https://www.linkedin.com
-                              </a>
-                           </span>
-                        </li>
-                        <li className="footer-info__contact-li">
-                           <span className="footer-info__contact-source">
-                              <FaFreeCodeCamp />
-                              Freelancehunt:
-                           </span>
-                           <span className="footer-info__contact-value">
-                              <a href="https://freelancehunt.com/freelancer/Yevhen1215.html">
-                                 https://freelancehunt.com
-                              </a>
-                           </span>
-                        </li>
-                     </ul>
-                     <h3 className="footer-info__made-in-ua">
-                        З гордістю зроблено в Україні 🇺🇦
-                     </h3>
+                     <h3>Копірайт © 2025 | Всі права захищені</h3>
                   </div>
                </div>
             </div>
